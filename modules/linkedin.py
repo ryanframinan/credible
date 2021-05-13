8e2681c26bdf9eb3f6678adb6fb646741f2485fc
ryanframinan <75705022+ryanframinan@users.noreply.github.com>
2021-05-13 09:13:53 -0700

    Update linkedin.py
    Edited to allow many matches appear

--- modules/linkedin.py
+++ modules/linkedin.py
@@ -1,24 +1,28 @@
-class LinkedIn:
+  class LinkedIn:
 
     def __init__(self, email):
         self.email = email
 
     def linkedin(self, email):
         self.email = email
-        success = False
+        allMatches = []
         i = 1
 
         while(i <= 37):
-            with open(r'\modules\files\linkedin\{}.txt'.format(i)) as f:
+            with open(r'FULLPATH\modules\files\linkedin\{}.txt'.format(i)) as f:
                 for num, line in enumerate(f, 1):
                     if email in line:
-                        print("[+] LinkedIn Credentials Found:\t", line)
-                        success = True
-                    if success == True: break
+                        line = line.replace("\n", "")
+                        allMatches.append(line)
+
             i+=1
             f.close()
-        if success == False:
-            print('\033[1;91m[-]\033[0m Credentials are not in LinkedIn Databreach.')
+        if len(allMatches) != 0:
+            j = 0
+            while j < len(allMatches):
+                print("[+] LinkedIn Credentials Found:\t", allMatches[j])
+                j+=1
+        else: print('\033[1;91m[-]\033[0m Credentials are not in LinkedIn Databreach.')
 
 if __name__ == "__main__":
     l = LinkedIn(email)
